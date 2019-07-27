09f887a3ec77955b0f2889f8e0aee1c5007a2caf
krassm <mkrass2@gmx.at>
2019-07-27 17:07:10 +0200

    basic map editor page with positioning

--- src/app/contribute-map/contribute-map.component.ts
+++ src/app/contribute-map/contribute-map.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {latLng, tileLayer} from 'leaflet';
+import {icon, latLng, marker, tileLayer, Map, LeafletMouseEvent, LatLng} from 'leaflet';
+import {LeafletEventsDemoComponent} from '@asymmetrik/ngx-leaflet/src/demo/app/leaflet/events/events-demo.component';
 
 @Component({
   selector: 'app-contribute-map',
@@ -10,22 +11,85 @@ import {latLng, tileLayer} from 'leaflet';
 export class ContributeMapComponent implements OnInit {
   role: string;
   options: any;
-
+  lat: number;
+  lon: number;
+  eventLat: number;
+  eventLon: number;
+  layers: any;
+  center: any;
+  zoom: any;
+  positionIsSet = false;
+  eventPositionIsSet = false;
 
   constructor(public router: Router) {
     this.role =  this.router.getCurrentNavigation().finalUrl.toString();
+
   }
 
   ngOnInit() {
     this.options = {
       layers: [
-        tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-          {attribution: 'Tiles &copy; Esri &mdash; Source: Esri' })
+        tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' })
       ],
-      zoomControl: false,
-      zoom: 5,
-      center: latLng(0, 0)
+      zoom: 3,
+      center: latLng(48.266033, 8.350205)
     };
+
+    this.getPosition();
   }
 
+  setCustomPoint() {
+  }
+
+  onMapReady(map: Map) {
+    console.log('i repeat and repeat');
+    // tslint:disable-next-line:no-shadowed-variable
+    map.on('click', <LeafletMouseEvent>(e) => {
+      this.eventLat = e.latlng.lat;
+      this.eventLon = e.latlng.lng;
+      console.log('Se clicked position: ' + this.eventLat + ' ' + this.eventLon);
+    });
+  }
+
+  updateMarker() {
+    console.log('i updated the markker i promise with the following coordinates :');
+    this.layers = marker([this.eventLat, this.eventLon], {
+      // This is nessasary because the module hast problems with marker icons
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    });
+  }
+
+  getPosition() {
+    // asynchronous
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        this.center = latLng(this.lat, this.lon);
+        this.zoom = 11;
+        this.positionIsSet = true;
+      });
+    } else {
+      alert('Geolocation is not supported');
+    }
+  }
+
+  updateMap() {
+    this.layers = marker([this.lat, this.lon], {
+      // This is nessasary because the module hast problems with marker icons
+      icon: icon({
+        iconSize: [25, 41],
+        iconAnchor: [13, 41],
+        iconUrl: 'assets/marker-icon.png',
+        shadowUrl: 'assets/marker-shadow.png'
+      })
+    });
+    this.positionIsSet = false;
+   }
 }
